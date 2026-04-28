623662e2ad94e5a75ad084b7298535ba4024b1f8
Anh Le Hoang <anhlehoang2004@gmail.com>
2026-04-28 16:10:22 +0700

    Lesson 6 traffic distributioon

---
+++ scripts/traffic_distribution.js
@@ -0,0 +1,64 @@
+import http from 'k6/http' 
+import { sleep, check, group } from 'k6'
+import { htmlReport } from "https://raw.githubusercontent.com/benc-uk/k6-reporter/main/dist/bundle.js"
+
+const BASE_URL = __ENV.BASE_URL || 'https://test.k6.io'
+
+const TRAFFIC_SPLIT = {
+    home: 0.6,
+    blog: 0.2,
+    news: 0.2
+}
+
+export const options = {
+    stages: [
+        {duration: '10s', target: 5}, 
+        {duration: '20s', target: 10}, 
+        {duration: '10s', target: 7},
+        {duration: '15s', target: 16},
+        {duration: '10s', target: 30},
+        {duration: '15s', target: 25},
+        {duration: '15s', target: 20},
+        {duration: '10s', target: 10},
+        {duration: '5s', target: 0},
+    ],
+    thresholds: {
+        http_req_duration: ['p(95)<500'],
+        checks: ['rate>0.95']
+    }
+};
+
+export default function () {
+    const random = Math.random()
+    if (random < TRAFFIC_SPLIT.home){
+        group('Home Page Users', () => {
+            const res = http.get(BASE_URL)
+            check(res, {
+                'home page loaded': (r) => r.status === 200,
+            })
+        })
+    }
+    else if (random < TRAFFIC_SPLIT.home + TRAFFIC_SPLIT.blog){
+        group('Blog Page Users', () => {
+            const res = http.get(`${BASE_URL}/blog/`)
+            check(res, {
+                'blog page loaded': (r) => r.status === 200
+            })
+        })
+    }
+    else{
+        group('News Page Users', () => {
+            const res = http.get(`${BASE_URL}/news.php`)
+            check(res, {
+                'news page loaded': (r) => r.status === 200
+            })
+        })
+    }
+    sleep(Math.random() * 2 + 1);
+}
+
+export function handleSummary(data) {
+    return {
+        "reports/report_traffic_distribution.html": htmlReport(data),
+    }
+}
